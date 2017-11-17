66810cbf80c96d1088a04a0a930d6dfa329efdef
AndresPantoja <33704873+Kike1996@users.noreply.github.com>
2017-11-17 17:22:47 -0500

    punto y coma

--- app.js
+++ app.js
@@ -338,7 +338,7 @@ request.on('response', function(response) {
 				sendTextMessage(senderID,fulfillment.speech);
 			}
 			else {
-				const action = result.action;;
+				const action = result.action;
 				const parameters = result.parameters;
 				console.log('action: ', action);
 				console.log('parameters: ',parameters);
