9f06b4b0a2ca8748cc6f332045c8dbefe3e5eff0
AndresPantoja <33704873+Kike1996@users.noreply.github.com>
2017-11-17 16:24:56 -0500

    bot

--- app.js
+++ app.js
@@ -315,7 +315,7 @@ function handleMessage(currentUser, senderID, message, isEcho, messageId, appId,
     }
     else {
       sendTextMessage(senderID, messageText);
-	  sendToBot(senderID,messageText);
+	  sendToBot(senderID,messageText)
     }
   }
   else if (messageAttachments) {
@@ -324,7 +324,7 @@ function handleMessage(currentUser, senderID, message, isEcho, messageId, appId,
 }
 
 function sendToBot(senderID, message){
-	var request = app.textRequest(message, {
+	var request = bot.textRequest(message, {
     sessionId: senderID
 });
 
